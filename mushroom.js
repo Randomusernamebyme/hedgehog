7f09cad50899f88b4dcabb3d09e26fe884b61d96
Randomusernamebyme <jiayiannie@gmail.com>
2025-10-25 22:43:46 +0800

    Clean up code and add all mushroom images (mushroom1, mushroom2, mushroom3)

--- mushroom.js
+++ mushroom.js
@@ -18,13 +18,28 @@ class Mushroom {
         // 計分標記
         this.scored = false;
         
-        // 圖片載入
+        // 圖片載入 - 根據類型選擇不同蘑菇圖片
         this.image = new Image();
         this.imageLoaded = false;
         this.image.onload = () => {
             this.imageLoaded = true;
         };
-        this.image.src = 'assets/images/mushroom1.png';
+        
+        // 根據蘑菇類型選擇圖片
+        switch(this.type) {
+            case 'red':
+                this.image.src = 'assets/images/mushroom1.png';
+                break;
+            case 'brown':
+                this.image.src = 'assets/images/mushroom2.png';
+                break;
+            case 'purple':
+                this.image.src = 'assets/images/mushroom3.png';
+                break;
+            case 'yellow':
+                this.image.src = 'assets/images/mushroom1.png'; // 使用mushroom1作為黃色
+                break;
+        }
         
         // 根據類型設定特殊屬性
         this.setTypeProperties();
@@ -99,68 +114,6 @@ class Mushroom {
         }
 
         ctx.restore();
-    }
-    
-    // 繪製蘑菇帽
-    drawMushroomCap(ctx) {
-        const capGradient = ctx.createRadialGradient(
-            this.x + this.width/2, this.y + this.height * 0.4, 0,
-            this.x + this.width/2, this.y + this.height * 0.4, this.width/2
-        );
-        
-        let capColor, spotColor, spotCount;
-        
-        switch(this.type) {
-            case 'red':
-                capColor = '#FF4444';
-                spotColor = '#FFFFFF';
-                spotCount = 3;
-                break;
-            case 'brown':
-                capColor = '#8B4513';
-                spotColor = '#FFFFFF';
-                spotCount = 2;
-                break;
-            case 'purple':
-                capColor = '#8A2BE2';
-                spotColor = '#FFD700';
-                spotCount = 4;
-                break;
-            case 'yellow':
-                capColor = '#FFD700';
-                spotColor = '#FF4500';
-                spotCount = 5;
-                break;
-        }
-        
-        capGradient.addColorStop(0, this.lightenColor(capColor, 0.3));
-        capGradient.addColorStop(1, capColor);
-        ctx.fillStyle = capGradient;
-        
-        ctx.beginPath();
-        ctx.ellipse(this.x + this.width/2, this.y + this.height * 0.4, 
-                   this.width/2, this.height * 0.3, 0, 0, 2 * Math.PI);
-        ctx.fill();
-
-        // 繪製蘑菇斑點
-        ctx.fillStyle = spotColor;
-        for (let i = 0; i < spotCount; i++) {
-            const spotX = this.x + (i + 1) * this.width / (spotCount + 1);
-            const spotY = this.y + this.height * 0.3;
-            const spotSize = this.type === 'yellow' ? 4 : 3;
-            ctx.beginPath();
-            ctx.arc(spotX, spotY, spotSize, 0, 2 * Math.PI);
-            ctx.fill();
-        }
-    }
-    
-    // 顏色變亮函數
-    lightenColor(color, factor) {
-        const hex = color.replace('#', '');
-        const r = Math.min(255, parseInt(hex.substr(0, 2), 16) + (255 * factor));
-        const g = Math.min(255, parseInt(hex.substr(2, 2), 16) + (255 * factor));
-        const b = Math.min(255, parseInt(hex.substr(4, 2), 16) + (255 * factor));
-        return `rgb(${Math.floor(r)}, ${Math.floor(g)}, ${Math.floor(b)})`;
     }
 
     // 檢查是否離開螢幕
